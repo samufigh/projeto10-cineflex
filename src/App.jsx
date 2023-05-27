bcb45ab902b5d164fe5f0ee4cf417e535401f0c5
samufigh <samufigh21@gmail.com>
2023-05-27 20:39:20 -0300

    routes configuradas

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
 import SuccessPage from "./pages/SuccessPage/SuccessPage"
 import axios from 'axios';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
     axios.defaults.headers.common['Authorization'] = 'vguSnKykv6ny986srnfBfOBd';
 
     return (
         <>
-           <NavContainer>CINEFLEX</NavContainer>
+            <BrowserRouter>
+                <NavContainer>CINEFLEX</NavContainer>
 
-            <HomePage />
-            {/* <SeatsPage /> */}
-            {/* <SessionsPage /> */}
-            {/* <SuccessPage /> */}
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/sessoes/:idFilme" element={<SeatsPage/>}/>
+                    <Route path="/assentos/:idSessao" element={<SessionsPage/>}/>
+                    <Route path="/sucesso" element={<SuccessPage/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
